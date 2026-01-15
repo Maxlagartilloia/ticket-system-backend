0d2bb5dd1b1bcd2727356670b8cab69c0109c690
Maxlagartilloia <maxlagartilloia@gmail.com>
2026-01-14 23:25:29 -0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://ticket_system_db_vczi_user:wODSKanQHzho5f3jE1AL5RUKY8Vd23ed@dpg-d5k68g24d50c739hbiig-a/ticket_system_db_vcziel"
 
 engine = create_engine(
     DATABASE_URL,
