bcdd32552c129c11045959b8a2c742a243092f73
Maxlagartilloia <maxlagartilloia@gmail.com>
2026-01-14 23:11:11 -0500

    Refactor database connection and add session management
    Updated DATABASE_URL to use environment variable and added get_db function for session management.

--- app/database.py
+++ app/database.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://ticket_system_db_vczi_user:wODSKanQHzho5f3jE1AL5RUKY8Vd23ed@dpg-d5k68g24d50c739hbiig-a/ticket_system_db_vcziel"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://ticket_system_db_vczi_user:wODSKanQHzho5f3jE1AL5RUKY8Vd23ed@dpg-d5k68g24d50c739hbiig-a/ticket_system_db_vcziel"
+)
 
 engine = create_engine(
     DATABASE_URL,
@@ -15,3 +19,10 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
