7473e9c54f2722602a0de92931ab86dafc22252a
Maxlagartilloia <maxlagartilloia@gmail.com>
2026-01-14 23:05:03 -0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
+DATABASE_URL = "postgresql://ticket_system_db_vczi_user:wODSKanQHzho5f3jE1AL5RUKY8Vd23ed@dpg-d5k68g24d50c739hbiig-a/ticket_system_db_vcziel"
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
